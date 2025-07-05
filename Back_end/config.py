19c704e5421595cad94ef2a2cb80db1ce8648cff
prakashvelusamy-hcl <prakashvelusamy1999@gmail.com>
2025-07-05 18:40:27 +0530

    fix

--- Back_end/config.py
+++ Back_end/config.py
@@ -21,21 +21,22 @@ import os
 import boto3
 import json
 
-def get_secret():
-    secret_name = os.getenv("SECRET_NAME", "rds!db-71168077-35cc-4ad7-97fe-b2b67b9696ae")
-    region_name = os.getenv("AWS_REGION", "ap-south-1")
-
+def get_secret(secret_name, region_name="ap-south-1"):
     client = boto3.client("secretsmanager", region_name=region_name)
     response = client.get_secret_value(SecretId=secret_name)
     return json.loads(response['SecretString'])
 
-# Load secrets
-secret = get_secret()
+# Main RDS credentials secret (managed by RDS, can't be edited)
+rds_secret = get_secret(os.getenv("RDS_SECRET_NAME", "rds!db-71168077-35cc-4ad7-97fe-b2b67b9696ae"))
 
-DB_USERNAME = secret["username"]
-DB_PASSWORD = secret["password"]
-DB_HOST = secret["host"]
-DB_NAME = secret["dbname"]
+# Additional custom secret with host and dbname
+app_secret = get_secret(os.getenv("APP_SECRET_NAME", "my-db-host-secret"))
+
+DB_USERNAME = rds_secret["username"]
+DB_PASSWORD = rds_secret["password"]
+DB_HOST = app_secret["host"]
+DB_NAME = app_secret["dbname"]
 
 SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
