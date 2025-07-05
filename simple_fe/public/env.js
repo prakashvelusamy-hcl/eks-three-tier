42f7f5f2d622e5bde045f762d78cbca8eedd0ec8
prakashvelusamy-hcl <prakashvelusamy1999@gmail.com>
2025-07-05 22:38:07 +0530

    fix

---
+++ simple_fe/public/env.js
@@ -0,0 +1,3 @@
+window._env_ = {
+  REACT_APP_API_BASE_URL: "http://localhost:5000/api", // default for local dev
+};
--- simple_fe/src/App.js
+++ simple_fe/src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = "http://13.235.245.253:5000/api/employees";
+// const API_URL = "http://13.235.245.253:5000/api/employees";
+const API_URL = (window._env_ && window._env_.REACT_APP_API_BASE_URL) || "/api/employees";
 
 function App() {
   const [employees, setEmployees] = useState([]);
