5932684751f050fbe8af67bb219f1c5cb5f4b1d9
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-07-07 09:41:54 +0530

    fix

--- modules/terraform-aws-helm/main.tf
+++ modules/terraform-aws-helm/main.tf
@@ -5,11 +5,11 @@ data "aws_eks_cluster_auth" "cluster" {
 
 
 # Kubernetes Provider
-provider "kubernetes" {
-  host                   = var.cluster_endpoint
-  cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
+#provider "kubernetes" {
+#  host                   = var.cluster_endpoint
+#  cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
+#  token                  = data.aws_eks_cluster_auth.cluster.token
+#}
 
 # HELM Provider
 provider "helm" {
